188fa057a3016c957565b1dfd8b133c7c5bba354
Shaun Smith <darscan@gmail.com>
2019-04-30 14:23:01 +0100

    chore: add conversion scripts

---
+++ scripts/to-graph.ts
@@ -0,0 +1,22 @@
+#!/usr/bin/env ts-node
+// tslint:disable:no-console
+
+import * as fs from 'fs';
+import {legacy} from '../src';
+
+const [STDIN, STDOUT] = [0, 1];
+
+const pkgManager = process.argv[2];
+if (!pkgManager) {
+  console.error('requires package manager argument. e.g. npm');
+  console.log('usage: cat tree.json | npx ts-node to-graph.ts npm > graph.json');
+  process.exit(1);
+}
+
+async function go() {
+  const depTree = JSON.parse(fs.readFileSync(STDIN).toString());
+  const graph = await legacy.depTreeToGraph(depTree, pkgManager);
+  fs.writeSync(STDOUT, JSON.stringify(graph));
+}
+
+go().catch(console.error);
---
+++ scripts/to-tree.ts
@@ -0,0 +1,23 @@
+#!/usr/bin/env ts-node
+// tslint:disable:no-console
+
+import * as fs from 'fs';
+import {legacy, createFromJSON} from '../src';
+
+const [STDIN, STDOUT] = [0, 1];
+
+const pkgType = process.argv[2];
+if (!pkgType) {
+  console.error('requires package type argument. e.g. npm');
+  console.log('usage: cat graph.json | npx ts-node to-tree.ts npm > tree.json');
+  process.exit(1);
+}
+
+async function go() {
+  const graphData = JSON.parse(fs.readFileSync(STDIN).toString());
+  const graph = createFromJSON(graphData);
+  const tree = await legacy.graphToDepTree(graph, pkgType);
+  fs.writeSync(STDOUT, JSON.stringify(tree));
+}
+
+go().catch(console.error);
